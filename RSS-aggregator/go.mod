689c0cab54f89bd19952cb911be69d80885b7e09
ashish2508 <jha250805@gmail.com>
2025-06-07 03:33:31 +0530

    might remove vendor

--- RSS-aggregator/go.mod
+++ RSS-aggregator/go.mod
@@ -1,3 +1,8 @@
 module github.com/ashish2508/Me-go
 
 go 1.24.4
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- RSS-aggregator/main.go
+++ RSS-aggregator/main.go
@@ -1,9 +1,34 @@
 package main
+
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 )
 
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
 	
+	router := chi.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Starting server on port: %v", portString)
+	
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port:", portString)
 }
