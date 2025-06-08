8f8736c2bd214fbedcd6ca742b5f4af86fcdb4fb
ashish2508 <jha250805@gmail.com>
2025-06-08 16:01:38 +0530

    .

---
+++ RSS-aggregator/json.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+  dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failder to marshal JSON Response: %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
+}
--- RSS-aggregator/main.go
+++ RSS-aggregator/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -18,6 +19,16 @@ func main() {
 	}
 	
 	router := chi.NewRouter()
+	
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*"}, // Allow all origins
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:          300,
+	}))
+
 	srv := &http.Server{
 		Handler: router,
 		Addr: ":" + portString,
