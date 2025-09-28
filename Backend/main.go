22bb2e9f212e6deff6b96b5105ad9825cd594b3c
ashish2508 <jha250805@gmail.com>
2025-09-29 02:56:33 +0530

    regular

--- Backend/main.go
+++ Backend/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"github.com/ashish2508/Me-go/Backend/internal/app"
 	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ashish2508/Me-go/Backend/internal/app"
 )
 
 func main() {
@@ -10,6 +13,19 @@ func main() {
 	app, err := app.NewApplication()
 	if err != nil{
 		panic(err)
+	}
+	
+	app.Logger.Println("We are running out app")
+	
+	server := &http.Server{
+		Addr: ":8080",
+		IdleTimeout: time.Minute,
+		ReadTimeout: time.Second * 10,
+		WriteTimeout: 30 * time.Second,
+	}
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
 	}
 	
 	
