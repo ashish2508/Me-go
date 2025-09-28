3fd16bf57db444748134965fbf155d86e1be96fc
ashish2508 <jha250805@gmail.com>
2025-09-29 02:52:11 +0530

    regular

--- Backend/main.go
+++ Backend/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ashish2508/Me-go/Backend/internal/app"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Works")
+	app, err := app.NewApplication()
+	if err != nil{
+		panic(err)
+	}
+	
+	
 }
