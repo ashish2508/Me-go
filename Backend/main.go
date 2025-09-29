709900f19786a0e33846c7017a8020cde4af022a
ashish2508 <jha250805@gmail.com>
2025-09-29 19:18:20 +0530

    regular

--- Backend/main.go
+++ Backend/main.go
@@ -4,27 +4,33 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-
+	"flag"
 	"github.com/ashish2508/Me-go/Backend/internal/app"
 )
 
 func main() {
 	fmt.Println("Works")
+	
+	var port int 
+	flag.IntVar(&port, "port", 8080,"go backend server port")
+	flag.Parse()
+	
 	app, err := app.NewApplication()
 	if err != nil{
 		panic(err)
 	}
 	
-	app.Logger.Println("We are running our app")
 	
 	
 	http.HandleFunc("/health", HealthCheck)   
 	server := &http.Server{
-		Addr: ":8088",
+		Addr: fmt.Sprintf(":%d",port),
 		IdleTimeout: time.Minute,
 		ReadTimeout: time.Second * 10,
 		WriteTimeout: 30 * time.Second,
 	}
+	app.Logger.Printf("We are running on Port: %d\n",port)
+	
 	err = server.ListenAndServe()
 	if err != nil {
 		app.Logger.Fatal(err)
